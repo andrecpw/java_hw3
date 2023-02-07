f2f7e89213f9da8bc2eeb53304af97af7f8c5b19
andrecpw <andre_cpw@hotmail.com>
2023-02-07 22:04:54 +0100

    first_commit

---
+++ src/main/java/space/harbour/java_hw3/JavaHw3Application.java
@@ -0,0 +1,13 @@
+package space.harbour.java_hw3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaHw3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaHw3Application.class, args);
+	}
+
+}
---
+++ src/test/java/space/harbour/java_hw3/JavaHw3ApplicationTests.java
@@ -0,0 +1,13 @@
+package space.harbour.java_hw3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaHw3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
