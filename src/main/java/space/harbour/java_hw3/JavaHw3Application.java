52d85d3ee357fdab844bda76a9a4cd4f5ba3ebca
andrecpw <andre_cpw@hotmail.com>
2023-02-10 15:42:14 +0100

    reinitialized

--- src/main/java/space/harbour/java_hw3/JavaHw3Application.java
+++ src/main/java/space/harbour/java_hw3/JavaHw3Application.java
@@ -2,24 +2,12 @@ package space.harbour.java_hw3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
-@RestController
 public class JavaHw3Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavaHw3Application.class, args);
-	}
-
-	@GetMapping("/inbox/items")
-	public String getInboxItems() {
-		return '[{"Items": {"id": 123, "status": "completed"}}]';
-	}
-
-	@PostMapping("/inbox/items/{id}")
-	public String updateInboxItem(@PathVariable("id") int id, @RequestBody Item item) {
-		return "{id: " + item.getId() + ", status: " + item.getStatus() + "}";
 	}
 
 }
